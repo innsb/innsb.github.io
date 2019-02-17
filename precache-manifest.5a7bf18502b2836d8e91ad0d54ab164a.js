45a9bad9507c3493a731fe75c7b8fdec1b7fcd37
innsb <vd-box@ya.ru>
2019-02-17 15:17:39 +0400

    0.1.5

---
+++ precache-manifest.5a7bf18502b2836d8e91ad0d54ab164a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e3f3e18d2946b4883964",
+    "url": "/js/chunk-vendors.4e32eced.js"
+  },
+  {
+    "revision": "89fd8828e355f39133f8",
+    "url": "/js/app.40f22c68.js"
+  },
+  {
+    "revision": "9fa3acab2c4e5990a731",
+    "url": "/js/about.35407111.js"
+  },
+  {
+    "revision": "16643255d83ae03e65f5211d38b3a447",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "89fd8828e355f39133f8",
+    "url": "/css/app.dde1df84.css"
+  }
+];
